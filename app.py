3dab3881ec44bf2dc7ea5ad1cab6576352370bd7
Jasiya Thompson <jdt97@cornell.edu>
2021-09-16 16:32:29 -0700

    adding first files to dogchat

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def feed():
+    test_posts [
+    "I can't wait to go to the park today",
+    "I could really use a treat right now",
+    "Aren't naps the best?"
+    ]
+    return render_template('index.html', posts=test_posts)
