e3a2657761ec0f3a94d117f39949c2afffc69a08
Jasiya Thompson <jdt97@cornell.edu>
2021-10-02 15:13:26 -0700

    Updated app and added profile picture

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def feed():
+
+    post1 = {
+        "Text":"I can't wait to go to the park today",
+        "Name":"Melba",
+        "Username":"melba",
+        "LikeCount":10,
+        "CommentCount":4,
+        "DateTime": datetime(2021,7,1,17,0,0),
+        "Picture": "melba_profile.png"
+    }
+    post2 = {
+        "Text":"I could really go for a treat right now",
+        "Name":"Melba",
+        "Username":"melba",
+        "LikeCount":25,
+        "CommentCount":15,
+        "DateTime": datetime(2021,8,4,17,2,5),
+        "Picture": "melba_profile.png"
+    }
     test_posts= [
-    "I can't wait to go to the park today",
-    "I could really use a treat right now",
-    "Aren't naps the best?"
+
     ]
     return render_template('index.html', posts=test_posts)
