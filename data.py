998c802fdefa5d0d13582d419c82ebff78d55d06
acebk <81196056+acebk@users.noreply.github.com>
2021-12-24 21:21:27 -0500

    Updated data model to include profile page

--- data.py
+++ data.py
@@ -23,7 +23,7 @@ comment3= {
 }
 
 post1 = {
-    "post_id" = 1,
+    "post_id":1,
     "Text":"I can't wait to go to the park today",
     "Name":"Melba",
     "Username":"melba",
@@ -33,7 +33,7 @@ post1 = {
     "Picture": "melba_profile.png"
 }
 post2 = {
-    "post_id" = 2, 
+    "post_id":2,
     "Text":"I could really go for a treat right now",
     "Name":"Melba",
     "Username":"melba",
@@ -47,3 +47,49 @@ test_posts = {
     1 : post1,
     2 : post2
 }
+
+melba_posts = {
+    1:post1
+    2:post2
+}
+
+jenna_posts = {
+
+}
+
+charlie_posts = {
+
+}
+
+melba{
+    "Name": "Melba",
+    "Bio":"My name is Melba. I'm a miniature golden-doodle.  My favorite place in the world is Discovery Park in Seattle, Washington.",
+    "Username": :"melba",
+    "Picture": "melba_profile.png",
+    "Birthday":datetime(2013, 2, 14),
+    "Posts": melba_posts
+}
+
+charlie{
+    "Name": "Charlie",
+    "Bio": "Hi, I'm Charlie!  I'm a standard poodle and I love to nap.",
+    "Username": "chucky",
+    "Picture": "charlie_profile_picture.jpg",
+    "Birthday": datetime(2018, 1, 2),
+    "Posts": charlie_posts
+}
+
+jenna{
+    "Name": "Jenna",
+    "Bio": "Hello, I'm Jenna. I'm a young pup with big aspirations",
+    "Username": "jennnnn",
+    "Picture": "charlie_profile_picture.jpg",
+    "Birthday": datetime(2021,5,6),
+    "Posts" : jenna_posts
+}
+
+dogs{
+    "melba":melba,
+    "chucky": charlie,
+    "jennnnn": jenna
+    }
